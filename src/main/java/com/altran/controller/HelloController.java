c252bf0bac090f20ad28e6ece9de25ebcecc5938
Achint Satsangi <achint.satsangi@gmail.com>
2019-09-30 14:21:56 +0200

    Run npm build during packaging and place in the public folder

--- src/main/java/com/altran/controller/HelloController.java
+++ src/main/java/com/altran/controller/HelloController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    public static String WELCOME_MESSAGE = "Hello, I'm the new plank app!!";
+    static String WELCOME_MESSAGE = "Hello, I'm the new plank app!!";
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String hello(){
         return WELCOME_MESSAGE;
     }
