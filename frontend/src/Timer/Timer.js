b5e639afcb8f73ea2f35f1ca31d3ed9b55aa140d
Melissa Ranjbarrad <m.ranjbarrad@gmail.com>
2019-10-01 10:33:08 +0200

    Remove unused properties in timer component

--- frontend/src/Timer/Timer.js
+++ frontend/src/Timer/Timer.js
@@ -13,35 +13,19 @@ export default class Timer extends Component {
         return (
             <>
                 <TimerMachine
-                    timeStart={60 * 1000} // start at 10 seconds
-                    timeEnd={0} // end at 20 seconds
+                    timeStart={0} 
+                    timeEnd={0} 
                     started={this.props.started}
                     paused={this.props.paused}
-                    countdown={this.props.countdown} // use as stopwatch
-                    interval={1000} // tick every 1 second
+                    countdown={this.props.countdown} 
+                    interval={1000} 
                     formatTimer={(time, ms) =>
                         moment.duration(ms, "milliseconds").format("h:mm:ss")
-                    }
-                    onStart={time =>
-                        console.info(`Timer started: ${JSON.stringify(time)}`)
                     }
                     onStop={time =>
                         console.info(`Timer stopped: ${JSON.stringify(time)}`)
                     }
-                    onTick={time =>
-                        console.info(`Timer ticked: ${JSON.stringify(time)}`)
-                    }
-                    onPause={time =>
-                        console.info(`Timer paused: ${JSON.stringify(time)}`)
-                    }
-                    onResume={time =>
-                        console.info(`Timer resumed: ${JSON.stringify(time)}`)
-                    }
-                    onComplete={time =>
-                        console.info(`Timer completed: ${JSON.stringify(time)}`)
-                    }
                 />
-
             </>
         )
     }
