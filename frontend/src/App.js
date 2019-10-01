40601bd217c0eb1ba6094781155635d00dae8911
Melissa Ranjbarrad <m.ranjbarrad@gmail.com>
2019-10-01 10:04:07 +0200

    Use react-timer-machine

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Timer from './Timer/Timer.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+let map = {
+  Start: 'success',
+  Pause: 'outline-light'
+}
+
+const Button = ( props ) => {
+  let btnType = map[props.label];
+  let className = ['btn', `btn-${btnType}`, 'mr-1'].join(' ');
+  return < button onClick={props.handleClick} className={className}>{props.label}</button>;
+}
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      paused: false,
+      started: false
+    };
+  }
+
+  render() {
+    const { started, paused } = this.state;
+
+    const toggleStartTimer = () => {
+      this.setState({
+        started: !this.state.started
+      });
+    }
+
+    const togglePauseTimer = () => {
+      this.setState({
+        paused: !this.state.paused
+      });
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Timer started={started} paused={paused} />
+          <div className="d-flex">
+            <Button handleClick={toggleStartTimer} label="Start" />
+            <Button handleClick={togglePauseTimer} label="Pause" />
+          </div>
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
