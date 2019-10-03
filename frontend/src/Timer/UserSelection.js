12901a21f5eea512bbc4f1fed0c819bbd575c838
Melissa Ranjbarrad <m.ranjbarrad@gmail.com>
2019-10-03 13:19:53 +0200

    Add component for user selection

---
+++ frontend/src/Timer/UserSelection.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+const axios = require("axios");
+
+export default class UserSelection extends Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        users: []
+      };
+    }
+  
+    componentDidMount() {
+      axios
+        .get("http://localhost:8080/users")
+        .then(response => {
+          this.setState({ users: response.data });
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  
+    render() {
+      const options = this.state.users.map(user => (
+        <option key={user}>{user}</option>
+      ));
+      return <select className="form-control">{options}</select>;
+    }
+  }
