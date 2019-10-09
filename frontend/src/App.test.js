e82ee71cf9cf040aa0d31113db30d84a1e93b148
Achint Satsangi <achint.satsangi@gmail.com>
2019-10-09 12:19:27 +0200

    Remove unused import

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import Chart from "chart.js";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
