a85ee634fa96bc0a497a806edb9480d88165620c
Achint Satsangi <achint.satsangi@gmail.com>
2019-10-09 12:02:50 +0200

    Take out logic of setting graph colors in the app
    We are now using a plugin chartjs-plugin-colorschemes to generate and
    assign colors automatically

--- src/main/java/com/altran/user/User.java
+++ src/main/java/com/altran/user/User.java
@@ -4,24 +4,14 @@ import java.util.NoSuchElementException;
 import java.util.stream.Stream;
 
 public enum User {
-    RUBEN("#9CF196"),
-    ACHINT("#ECEBA7"),
-    MELISSA("#EDAAAA"),
-    SAINYAM("#b1e8ed"),
-    OLE("#01D28E"),
-    HENRIK("#FE59D7"),
-    PK("#eccd8f"),
-    CAMILLA("#DA9833");
-
-    private final String color;
-
-    User(String color) {
-        this.color = color;
-    }
-
-    public String getColor() {
-        return color;
-    }
+    RUBEN,
+    ACHINT,
+    MELISSA,
+    SAINYAM,
+    OLE,
+    HENRIK,
+    PK,
+    CAMILLA;
 
     public static User of(String userString) {
         return Stream.of(User.values())
